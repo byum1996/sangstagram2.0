34cbe2c17f5d7d951da486e7258b946f2a3f6c4a
Brandon Yum <byum1996@gmail.com>
2021-04-22 20:10:24 -0700

    MatUI makeStyles implemented to put post toolbar icons align right

--- src/profile/postThumbnail.js
+++ src/profile/postThumbnail.js
@@ -7,14 +7,24 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Slide from '@material-ui/core/Slide';
+import { makeStyles } from "@material-ui/core/styles";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
+const useStyles = makeStyles({
+    rightToolbar: {
+      marginLeft: "auto",
+      marginRight: -12
+    },
+  })
+
 const PostThumbnail = ({post, handleOnDeletePost}) => {
     const { photoUrl } = post
     const [openState, setOpenState] = useState(false);
+
+    const classes = useStyles()
 
     const handleOnClickOpen = () => {
         setOpenState(true);
@@ -33,21 +43,20 @@ const PostThumbnail = ({post, handleOnDeletePost}) => {
                 src={photoUrl}
             />
             <Dialog fullScreen open={openState} TransitionComponent={Transition}>
-                <AppBar>
+                <AppBar position='static'>
                     <Toolbar>
                         <IconButton edge="start" color="inherit" onClick={handleOnClickClose} aria-label="close">
                             <CloseIcon />
                         </IconButton>
-                        <IconButton edge="end" color='inherit' onClick={()=> handleOnDeletePost(post.id)} aria-label="remove">
-                            <DeleteIcon />
-                        </IconButton>
+                        <div className={classes.rightToolbar}>
+                            <IconButton color='inherit' onClick={()=> handleOnDeletePost(post.id)} aria-label="remove">
+                                <DeleteIcon />
+                            </IconButton>
+                        </div>
                     </Toolbar>
                 </AppBar>
-                <Post 
-                    post={post} 
-                />
+                <Post post={post} />
             </Dialog>
-
         </>
     )
 }
